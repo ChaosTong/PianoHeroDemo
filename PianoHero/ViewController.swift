d903b7a034c63fbc8047f638f11f3469de0a0b2d
ChaosTong <easyulife@gmail.com>
2023-07-07 13:54:10 +0800

    Initial Commit

---
+++ PianoHero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PianoHero
+//
+//  Created by slaver on 2023/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
